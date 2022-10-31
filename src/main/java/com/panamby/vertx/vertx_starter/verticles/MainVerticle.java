534114bacbe7567a2fa04a03d0191f7d43e2adba
Rafael <rafael.souza@prill.com.br>
2022-10-31 15:35:34 -0300

    Verticle Config

--- src/main/java/com/panamby/vertx/vertx_starter/verticles/MainVerticle.java
+++ src/main/java/com/panamby/vertx/vertx_starter/verticles/MainVerticle.java
@@ -1,9 +1,12 @@
 package com.panamby.vertx.vertx_starter.verticles;
 
+import java.util.UUID;
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -19,7 +22,13 @@ public class MainVerticle extends AbstractVerticle {
 		vertx.deployVerticle(new VerticleA());
 		vertx.deployVerticle(new VerticleB());
 		vertx.deployVerticle(VerticleN.class.getName(),
-				new DeploymentOptions().setInstances(4));
+				new DeploymentOptions()
+					.setInstances(4)
+					.setConfig(new JsonObject()
+							.put("id", UUID.randomUUID().toString())
+							.put("name", VerticleN.class.getName())
+					)
+				);
 		startPromise.complete();
 	}
 }

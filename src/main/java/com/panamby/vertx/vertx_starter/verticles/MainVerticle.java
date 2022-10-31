2f7965e3d8d62d19b654397687db2b90e7453d0c
Rafael <rafael.souza@prill.com.br>
2022-10-31 16:00:16 -0300

    Logging

--- src/main/java/com/panamby/vertx/vertx_starter/verticles/MainVerticle.java
+++ src/main/java/com/panamby/vertx/vertx_starter/verticles/MainVerticle.java
@@ -6,9 +6,13 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import io.vertx.core.json.JsonObject;
 
 public class MainVerticle extends AbstractVerticle {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
 
 	public static void main(String[] args) {
 
@@ -18,7 +22,7 @@ public class MainVerticle extends AbstractVerticle {
 
 	@Override
 	public void start(Promise<Void> startPromise) throws Exception {
-		System.out.println("Start " + getClass().getName());
+		LOG.debug("Start {}", getClass().getName());
 		vertx.deployVerticle(new VerticleA());
 		vertx.deployVerticle(new VerticleB());
 		vertx.deployVerticle(VerticleN.class.getName(),

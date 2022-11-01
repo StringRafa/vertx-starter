8834561f88e04a19e9a1fd7ae3e7737278e1b9e3
Rafael <rafael.souza@prill.com.br>
2022-11-01 15:08:47 -0300

    The JSON Array

--- src/test/java/com/panamby/vertx/vertx_starter/json/JsonObjectExample.java
+++ src/test/java/com/panamby/vertx/vertx_starter/json/JsonObjectExample.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
 class JsonObjectExample {
@@ -37,5 +38,19 @@ class JsonObjectExample {
 		assertEquals(1, asJsonObject.getInteger("id"));
 		assertEquals("Alice", asJsonObject.getString("name"));
 		assertEquals(true, asJsonObject.getBoolean("loves_vertx"));
+	}
+
+	@Test
+	void jsonArrayCanBeMapped() {
+		
+		final JsonArray myJsonArray = new JsonArray();
+		myJsonArray
+			.add(new JsonObject().put("id", 1))
+			.add(new JsonObject().put("id", 2))
+			.add(new JsonObject().put("id", 3))
+			.add("randomValue");
+		
+		assertEquals("[{\"id\":1},{\"id\":2},{\"id\":3},\"randomValue\"]", myJsonArray.encode());
+		
 	}
 }

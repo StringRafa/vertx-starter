2f43a50078763e7bcaf5f117e3d0904094ac8160
Rafael <rafael.souza@prill.com.br>
2022-11-03 16:57:44 -0300

    Vertx Docker - Jib

--- src/main/java/com/panamby/vertx/vertx_starter/MainVerticle.java
+++ src/main/java/com/panamby/vertx/vertx_starter/MainVerticle.java
@@ -1,10 +1,16 @@
 package com.panamby.vertx.vertx_starter;
 
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
-import io.vertx.core.Vertx;
 
 public class MainVerticle extends AbstractVerticle {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
@@ -19,11 +25,12 @@ public class MainVerticle extends AbstractVerticle {
       } else {
         startPromise.fail(http.cause());
       }
+      vertx.setPeriodic(100, id -> LOG.info("" + new Random().nextDouble()));
     });
   }
   
-  public static void main(String[] args) {
-	  var vertx = Vertx.vertx();
-	  vertx.deployVerticle(new MainVerticle());
-  }
+//  public static void main(String[] args) {
+//	  var vertx = Vertx.vertx();
+//	  vertx.deployVerticle(new MainVerticle());
+//  }
 }
